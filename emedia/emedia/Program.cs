e2e34137f62c4c39881beffaa79357271801a26c
Kamil Kielbasa <kamilkielbasa73@gmail.com>
2019-05-12 12:12:18 +0200

    Added WAV Writer class.

--- emedia/emedia/Program.cs
+++ emedia/emedia/Program.cs
@@ -10,11 +10,17 @@ namespace emedia
     {
         static void Main(string[] args)
         {
-            string pathToWAVFile = @"C:\Users\kielbkam\Desktop\semestr 6\emedia\emedia\emedia\Yamaha-V50-Rock-Beat-120bpm.wav";
+            string path = @"C:\Users\kielbkam\Desktop\semestr 6\emedia\emedia\emedia\";
+            string file1 = "Yamaha-V50-Rock-Beat-120bpm.wav";
+            string file2 = "Yamaha-V50-Rock-Beat-120bpm2.wav";
 
-            WAVFile wavFile = new WAVReader(pathToWAVFile).ReadWAVFile();
+            WAVFile wavFile = new WAVReader(path + file1).ReadWAVFile();
             wavFile.DisplayHeader();
-           
+
+            WAVWriter wavWriter = new WAVWriter(path + file2);
+            wavWriter.WriteWAVFile(wavFile);
+
+            Console.WriteLine("Finished");
             Console.ReadKey();
         }
     }
---
+++ emedia/emedia/WAVWriter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emedia
+{
+    public class WAVWriter
+    {
+        private string pathToWAVFile { get; set; }
+
+        public WAVWriter(string pathToWAVFile)
+        {
+            this.pathToWAVFile = pathToWAVFile;
+        }
+
+        private int ReverseEndianness(int value)
+        {
+            return BinaryPrimitives.ReverseEndianness(value);
+        }
+
+        private int ConvertStringToInt(string str)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(str);
+            Array.Reverse(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public void WriteWAVFile(WAVFile wavFile)
+        {
+            using (FileStream fs = new FileStream(this.pathToWAVFile, FileMode.OpenOrCreate))
+            {
+                BinaryWriter binaryWriter = new BinaryWriter(fs);
+
+                binaryWriter.Write(ReverseEndianness(ConvertStringToInt(wavFile.ChunkId)));
+                binaryWriter.Write(wavFile.ChunkSize);
+                binaryWriter.Write(ReverseEndianness(ConvertStringToInt(wavFile.Format)));
+                binaryWriter.Write(ReverseEndianness(ConvertStringToInt(wavFile.Subchunk1ID)));
+                binaryWriter.Write(wavFile.Subchunk1Size);
+                binaryWriter.Write((UInt16)wavFile.AudioFormat);
+                binaryWriter.Write((UInt16)wavFile.NumChannels);
+                binaryWriter.Write(wavFile.SampleRate);
+                binaryWriter.Write(wavFile.ByteRate);
+                binaryWriter.Write((Int16)wavFile.BlockAlign);
+                binaryWriter.Write((UInt16)wavFile.BitsPerSample);
+                binaryWriter.Write(ReverseEndianness(ConvertStringToInt(wavFile.Subchunk2ID)));
+                binaryWriter.Write(wavFile.Subchunk2Size);
+                binaryWriter.Write(wavFile.Data);
+            }
+        }
+    }
+}
